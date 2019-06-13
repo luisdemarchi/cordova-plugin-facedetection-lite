c343077fa1b5c16e539d461eb6fe37f13bf62130
Luis De Marchi <luis@luisdemarchi.com.br>
2019-06-13 20:53:37 -0300

    full browser platform

--- src/browser/FaceDetectorProxy.js
+++ src/browser/FaceDetectorProxy.js
@@ -1,8 +1,61 @@
 var cordovaProxy = require("cordova/exec/proxy");
 
+function initFaceDetector(success, error, data) {
+    // { sizeFrameMemory: sizeFrameMemory, faceFinderPath: faceFinderPath }
+    var self = this;
+    let imported = document.createElement('script');
+    imported.src = 'pico.min.js';
+    document.head.appendChild(imported);
+
+    let faceFrameMemory = window.pico.instantiate_detection_memory(data.sizeFrameMemory);
+
+    fetch(data.faceFinderPath).then(function (response) {
+        response.arrayBuffer().then(function (buffer) {
+            var bytes = new Int8Array(buffer);
+            let faceFinderClassifyRegion = pico.unpack_cascade(bytes);
+
+            success({faceFrameMemory: faceFrameMemory, faceFinderClassifyRegion: faceFinderClassifyRegion});
+        })
+    })
+};
+
+function detections(success, error, data) {
+    // { rgbaX: rgbaX, height: height, width: width, params: params, faceFinderClassifyRegion: faceFinderClassifyRegion }
+    let image = {
+        "pixels": _convertRgbaToGrayscale(data.rgba, data.height, data.width),
+        "nrows": data.height,
+        "ncols": data.width,
+        "ldim": data.width
+    }
+
+    var dets = window.pico.run_cascade(image, data.faceFinderClassifyRegion, data.params);
+
+    success(dets);
+};
+
+function cluster(success, error, data) {
+    // { iouthreshold: iouthreshold, detections: self.dets }
+    let dets = window.pico.cluster_detections(data.detections, data.iouthreshold);
+    success(dets);
+};
+
+function _convertRgbaToGrayscale(rgba, nrows, ncols) {
+    var gray = new Uint8Array(nrows * ncols);
+    for (var r = 0; r < nrows; ++r)
+        for (var c = 0; c < ncols; ++c)
+            // gray = 0.2*red + 0.7*green + 0.1*blue
+            gray[r * ncols + c] =
+                (2 * rgba[r * 4 * ncols + 4 * c + 0] +
+                    7 * rgba[r * 4 * ncols + 4 * c + 1] +
+                    1 * rgba[r * 4 * ncols + 4 * c + 2]) /
+                10;
+    return gray;
+}
 
 module.exports = {
-
+    initFaceDetector: initFaceDetector,
+    detections: detections,
+    cluster: cluster
 };
 
 cordovaProxy.add("FaceDetector", module.exports);
--- www/FaceDetector.js
+++ www/FaceDetector.js
@@ -1,57 +1,53 @@
 var exec = require('cordova/exec');
 
-function init(result){
-    // this.faceFrameMemory = window.pico.instantiate_detection_memory(30);
-    //   this.faceFinderClassifyRegion = function(r, c, s, pixels, ldim) {
-    //     return -1.0;
-    //   };
-    // donwload file
+var exec = require('cordova/exec');
+
+var faceFrameMemory;
+var faceFinderClassifyRegion;
+var initialized = false;
+
+function initFaceDetector(sizeFrameMemory, faceFinderPath, result) {
+    console.log("I0 -> 1");
+
+    if (initialized) {
+        result(false);
+        return;
+    }
+
+    faceFinderClassifyRegion = function (r, c, s, pixels, ldim) {
+        return -1.0;
+    };
+    var callback = function (data) {
+        faceFrameMemory = data.faceFrameMemory;
+        faceFinderClassifyRegion = data.faceFinderClassifyRegion;
+
+        result(true);
+    }
+
+    exec(callback, callback, "FaceDetector", "initFaceDetector", { sizeFrameMemory: sizeFrameMemory, faceFinderPath: faceFinderPath });
 };
 
-function detections(rgba, height, width, result){
-    // image = {
-    //     "pixels": rgba_to_grayscale(rgba, height, width),
-    //     "nrows": height,
-    //     "ncols": width,
-    //     "ldim": width
-    // }
-    // params = {
-    //     "shiftfactor": 0.1, // move the detection window by 10% of its size
-    //     "minsize": width*0.4,     // minimum size of a face
-    //     "maxsize": width,    // maximum size of a face
-    //     "scalefactor": 1.1  // for multiscale processing: resize the detection window by 10% when moving to the higher scale
-    // }
-    // // run the cascade over the frame and cluster the obtained detections
-    // // dets is an array that contains (r, c, s, q) quadruplets
-    // // (representing row, column, scale and detection score)
-    // dets = pico.run_cascade(image, facefinder_classify_region, params);
-    // dets = update_memory(dets);
-    // dets = pico.cluster_detections(dets, 0.3);
+function detections(rgba, height, width, minSizeFace, maxSizeFace, iouthreshold, result) {
+    var params = {
+        "shiftfactor": 0.1,
+        "minsize": minSizeFace,
+        "maxsize": maxSizeFace,
+        "scalefactor": 1.1
+    }
+
+    var cluster = function (_dets) {
+        result(_dets);
+    }
+
+    var detections = function (_dets) {
+        var dets = faceFrameMemory(_dets);
+        exec(cluster, cluster, "FaceDetector", "cluster", { iouthreshold: iouthreshold, detections: dets });
+    }
+
+    exec(detections, detections, "FaceDetector", "detections", { rgba: rgba, height: height, width: width, params: params, faceFinderClassifyRegion: faceFinderClassifyRegion });
 }
 
-function instantiateDetectionMemory(size, result) {
-    exec(success, error, "FaceDetector", "ActiveView", [name, top, left, width, height, data]);
-};
-
-function unpackCascade(bytes, result) {
-    exec(success, error, "FaceDetector", "CreateView", [name, data]);
-};
-
-function runCascade(image, params) {
-    exec(success, error, "FaceDetector", "RestartView", [name]);
-};
-
-
 module.exports = {
-  RestartView: RestartView,
-  SendViewToBack: SendViewToBack,
-  BringViewToFront: BringViewToFront,
-  ActiveView: ActiveView,
-  CreateView: CreateView,
-  MoveView: MoveView,
-  ShowView: ShowView,
-  HideView: HideView,
-  DeleteView: DeleteView,
-  RemoveAllViews: RemoveAllViews,
-  SetURL: SetURL
+    initFaceDetector: initFaceDetector,
+    detections: detections
 };
