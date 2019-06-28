79c7807ec891bd3d1172f2811428251ee3da5fcf
Luis De Marchi <luis@luisdemarchi.com.br>
2019-06-28 18:10:22 -0300

    .gitignore edit

---
+++ src/android/obj/local/armeabi-v7a/objs/picornt/FaceDetectionJni.o.d
@@ -0,0 +1,7 @@
+./obj/local/armeabi-v7a/objs/picornt/FaceDetectionJni.o: \
+  jni/FaceDetectionJni.c jni/../../common//picornt/picornt.h \
+  jni/FaceDetectionJni.h
+
+jni/../../common//picornt/picornt.h:
+
+jni/FaceDetectionJni.h:
---
+++ src/android/obj/local/x86_64/objs/picornt/__/__/common/picornt/picornt.o.d
@@ -0,0 +1,2 @@
+./obj/local/x86_64/objs/picornt/__/__/common/picornt/picornt.o: \
+  jni/../../common/picornt/picornt.c
